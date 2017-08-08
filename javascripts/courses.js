1fae3cf23c11d68e4008be7641aa8b1ee96579f1
soe-j <soeda@val.co.jp>
2017-08-08 23:31:10 +0900

    search and logging result

--- javascripts/courses.js
+++ javascripts/courses.js
@@ -1,6 +1,4 @@
 $(function(){
-  // var ekispertConfig =
-
   var courses = new Vue({
     el: '#courses',
     data: {
@@ -9,7 +7,13 @@ $(function(){
     },
     methods: {
       search: function(){
-        console.log('検索');
+        var url = config.ekispert.url + '/search/course/extreme';
+        $.getJSON(url, {
+          key: config.ekispert.key,
+          viaList: [this.departure, this.arrive].join(':')
+        }, function(res){
+          console.log(res);
+        });
       }
     }
   });
