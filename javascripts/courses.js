ef44d47e82d891ea4716222b3ebf9812e95f1865
soe-j <soeda@val.co.jp>
2018-01-07 01:43:50 +0900

    output without recursion

--- javascripts/courses.js
+++ javascripts/courses.js
@@ -3,16 +3,18 @@ $(function(){
     el: '#courses',
     data: {
       departure: '高円寺',
-      arrive: '東京'
+      arrive: '東京',
+      ResultSet: {}
     },
     methods: {
       search: function(){
+        var self = this;
         var url = config.ekispert.url + '/search/course/extreme';
         $.getJSON(url, {
           key: config.ekispert.key,
           viaList: [this.departure, this.arrive].join(':')
         }, function(res){
-          console.log(res);
+          self.ResultSet = res.ResultSet;
         });
       }
     }
